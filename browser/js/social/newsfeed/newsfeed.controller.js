fe63934249d3881fd59c3e83141e49e6bf0fe930
Iris <icc.lany@gmail.com>
2016-05-05 18:29:03 -0400

    Newsfeed is up with a mediocre seed file

--- browser/js/social/newsfeed/newsfeed.controller.js
+++ browser/js/social/newsfeed/newsfeed.controller.js
@@ -1,4 +1,17 @@
 core.controller('NewsfeedCtrl', function($scope, posts) {
     $scope.posts = posts;
-    console.log("GOT THE POSTS!")
+
+    $scope.data = {
+      selectedIndex: 0,
+      secondLabel:   "Private Feed",
+      thirdLabel: "Other Private Feed"
+    };
+
+    $scope.next = function() {
+      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+    };
+    $scope.previous = function() {
+      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+    };
+
 });
