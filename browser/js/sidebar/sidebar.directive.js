4b7677c0722d42ad2e4c2c3295d014ba70f7fa85
Justin Isaf <code@justinisaf.com>
2016-05-19 13:29:34 -0400

    fixed some stuff

--- browser/js/sidebar/sidebar.directive.js
+++ browser/js/sidebar/sidebar.directive.js
@@ -1,4 +1,4 @@
-core.directive('blSidebar', function($state, $rootScope, AuthService, AUTH_EVENTS, SidebarFactory, TimerFactory, IterateFactory) {
+core.directive('blSidebar', function($state, $rootScope, ActionFactory, AuthService, AUTH_EVENTS, SidebarFactory, TimerFactory, IterateFactory) {
     return {
         restrict: 'E',
         scope: {
@@ -31,9 +31,8 @@ core.directive('blSidebar', function($state, $rootScope, AuthService, AUTH_EVENT
             scope.items = SidebarFactory.getLinks(scope.userLoggedIn);
 
             //need to clean up scope.localCtrl .... was originally used to link stuff
-            scope.localCtrl = scope.control || {};
-            let count = 0;
-            // scope.selectedLink = IterateFactory.linkValue;
+            // scope.localCtrl = scope.control || {};
+            scope.selectedLink = 0;
 
             // scope.$watch(function() {
             //     return IterateFactory.linkValue;
@@ -47,10 +46,15 @@ core.directive('blSidebar', function($state, $rootScope, AuthService, AUTH_EVENT
             $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
             $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
             $rootScope.$on("iterate", () => {
-                if($rootScope.nav){
-                    // console.log("iterating");
+                if(ActionFactory.nav){
                     scope.selectedLink = SidebarFactory.moveSelected();
                     scope.$digest();
+                }
+            })
+            $rootScope.$on("singleBlink", () => {
+                if(ActionFactory.nav){
+                    scope.selectedLink = null;
+                    SidebarFactory.changeState();
                 }
             })
         }
